f9a89b06371d5f3a8f39f22b8fb0c8ea519a3062
Amr Abd El Fattah <amrelfattah.dev@gmail.com>
2023-09-25 15:29:49 +0200

    Add a mock for type of activities

--- frontEnd-Sportsee/src/Services/ActivitiesPerformances.jsx
+++ frontEnd-Sportsee/src/Services/ActivitiesPerformances.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 
 export default async function ActivitiesPerformances(Id) {
-    const endpoint = `http://localhost:3000/user/${Id}/performance`;
+    const isDataMocked = false;
+    const endpoint = isDataMocked ? `../../src/Mock/ActivitiesPerformances.json`: `http://localhost:3000/user/${Id}/performance`;
   
     try {
       const response = await fetch(endpoint);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      const data = await response.json();
+      let data = await response.json();
       if (!data) return null
+      
+      if(isDataMocked){
+        const FoundData = data.find(el => el.userId === Number(Id))
+        data = { data: FoundData}
+      }
       return data;
     } catch (error) {
       console.error("Fetch error: ", error);
